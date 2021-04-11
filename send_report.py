265429c3a8a9969a1ba8f53d143f0494101b4966
Dominik Polzer <dominik.polzer@web.de>
2021-04-11 20:43:56 +0200

    Separate settings from send mail

--- send_report.py
+++ send_report.py
@@ -59,34 +59,45 @@ def create_visualization(x,y):
 
 path_plot, dir = create_visualization(x,y)
 
-# define path for header img
+# path to plot image
+filename = str(date.today()) + ".png"
+dir = pathlib.Path(__file__).parent.absolute()
+folder = r"/results/"
+path = str(dir) + folder + filename
+
+# path to header image
 folder_img = r"/img/"
 path_img =  str(dir) + folder_img + "header_img.png"
 
-def send_email(path_plot,path_img,dir):
+# Settings
+from_mail = os.environ['from_mail']  # "test.name@googlemail.com"
+from_password = os.environ['G-PW']  # "password123"
+to_mail = os.environ['to_mail']  # "test@outlook.com"
+smtp_server = "smtp.gmail.com"
+smtp_port = 465
+
+def send_email(path_plot, smtp_server, smtp_port, from_mail, from_password, to_mail):
     '''
         Send results via email
     '''
 
     COMMASPACE = ', '
 
-    g_secret = os.environ['G-PW']
-    mail1 = os.environ['MAIL1']
-    mail2 = os.environ['MAIL2']
-
-    gmail_user = mail1
-    gmail_password = g_secret
+    from_password = os.environ['G-PW']
+    from_mail = os.environ['from_mail']
+    to_mail = os.environ['to_mail']
 
     # Create the container (outer) email message.
     msg = MIMEMultipart()
     msg['Subject'] = 'Simple Data Report: Time analysis'
-    msg['From'] = gmail_user
-    msg['To'] = COMMASPACE.join([mail1, mail2])
+    msg['From'] = from_mail
+    msg['To'] = COMMASPACE.join([from_mail, to_mail])
     msg.preamble = 'Simple Data Report: Time analysis'
 
     # Open the files in binary mode.  Let the MIMEImage class automatically
     # guess the specific image type.
-    with open(path_plot, 'rb') as fp:
+    with open(path, 'rb') as fp:
+        #fp = open(path, 'rb')
         img = MIMEImage(fp.read())
         img.add_header('Content-Disposition', 'attachment', filename='hours_plot.png')
         img.add_header('X-Attachment-Id', '0')
@@ -95,6 +106,7 @@ def send_email(path_plot,path_img,dir):
         msg.attach(img)
 
     with open(path_img, 'rb') as fp:
+        #fp = open(path, 'rb')
         img = MIMEImage(fp.read())
         img.add_header('Content-Disposition', 'attachment', filename='header.png')
         img.add_header('X-Attachment-Id', '1')
@@ -106,17 +118,18 @@ def send_email(path_plot,path_img,dir):
     f = codecs.open(str(dir) + "/email.html", 'r')
     string = f.read()
 
-    # Replace path with cid of attached files
+    # Replace the relative path to images with ContentID
     html_string = string.replace("./results/2021-04-08.png", "cid:0")
     html_string = html_string.replace("./img/header_img.png", "cid:1")
+
     msg.attach(MIMEText(html_string, 'html', 'utf-8'))
 
     # Send the email via our own SMTP server
-    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    server = smtplib.SMTP_SSL(smtp_server, smtp_port)
     server.ehlo()
-    server.login(gmail_user, gmail_password)
+    server.login(from_mail, from_password)
 
-    server.sendmail(gmail_user, [mail1, mail2], msg.as_string())
+    server.sendmail(from_mail, [from_mail, to_mail], msg.as_string())
     server.quit()
 
-send_email(path_plot,path_img,dir)
+send_email(path_plot, smtp_server, smtp_port, from_mail, from_password, to_mail)
